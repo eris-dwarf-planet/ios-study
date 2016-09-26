f268eede7fbcaacfe45f06ca32f97c6b90bbe88a
eris-dwarf-planet <bo.od.com001@gmail.com>
2016-09-26 21:12:22 +0900

    add study-janken

---
+++ study_janken/study_janken/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  study_janken
+//
+//  Created by kanazawa masahito on 2016/09/26.
+//  Copyright © 2016年 none. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
